4af4f2377c1184b7c38fd8fc0901485d2048a5c0
zakeer3551 <zak.ican4d@gmail.com>
2026-03-12 01:37:05 +0530

    deploying roboshop using for terraform full project

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 component=$1
+environment=$2
 dnf install ansible -y
 
 
@@ -10,4 +11,4 @@ git clone https://github.com/Zakeer3551/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e "component=$component" roboshop.yaml
+ansible-playbook -e "component=$component" -e "environment=$environment" roboshop.yaml
