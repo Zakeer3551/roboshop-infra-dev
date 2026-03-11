65eb12026666dd05e4b8b3680daa4181d6709a08
zakeer3551 <zak.ican4d@gmail.com>
2026-03-11 23:08:56 +0530

    deploying roboshop using for terraform full project

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "mongodb" {
   name    = "mongodb-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = "1"
-  records = "mongodb-${var.environment}.${var.domain_name}"
+  records = [aws_instance.mongodb.private_ip]
   allow_overwrite = true
 }
 
@@ -12,16 +12,7 @@ resource "aws_route53_record" "redis" {
   name    = "redis-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = "1"
-  records = "redis-${var.environment}.${var.domain_name}"
-  allow_overwrite = true
-}
-
-resource "aws_route53_record" "user" {
-  zone_id = var.zone_id
-  name    = "user-${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = "1"
-  records = "user-${var.environment}.${var.domain_name}"
+  records = [aws_instance.redis.private_ip]
   allow_overwrite = true
 }
 
