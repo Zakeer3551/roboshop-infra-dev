a3a57a1d5afa55f9e999990c9c076c912054d2ec
zakeer3551 <zak.ican4d@gmail.com>
2026-03-11 23:36:21 +0530

    deploying roboshop using for terraform full project

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -52,14 +52,12 @@ resource "aws_security_group_rule" "redis_bastion" {
   security_group_id = local.redis_sg_id
 }
 
-
-/* 
-resource "aws_security_group_rule" "user_redis" {
+resource "aws_security_group_rule" "mysql_bastion" {
   type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
+  from_port         = 22
+  to_port           = 22
   protocol          = "tcp"
-  source_security_group_id = local.user_sg_id
-  security_group_id = local.mongodb_sg_id
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.mysql_sg_id
 }
- */
+
