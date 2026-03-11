ce351d0976ccb69688d5e6372d4c4933f149285a
zakeer3551 <zak.ican4d@gmail.com>
2026-03-12 00:31:37 +0530

    deploying roboshop using for terraform full project

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -13,7 +13,7 @@ locals {
   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
 
-  mysql_role = join("-", [for name in ["${var.project}", "${var.var.environment}", "mysql" ] : title(name)])
+  mysql_role = join("-", [for name in ["${var.project}", "${var.environment}", "mysql" ] : title(name)])
   /* mysql_role = join("-"", 
     [for name in ["${var.project}", "${var.var.environment}", "mysql" ] : title(name)]
     ) */
