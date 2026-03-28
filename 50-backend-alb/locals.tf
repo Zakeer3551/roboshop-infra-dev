88c0c55610e94a21e3748387f850b224af052b47
zakeer3551 <zak.ican4d@gmail.com>
2026-03-28 16:18:41 +0530

    deploying roboshop full project using terraform

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -8,5 +8,5 @@ locals {
   }
 
   backend_sg_id = data.aws_ssm_parameter.backend_sg_id.value
-  private_subnet_ids = split(",",  data.aws_ssm_parameter.private_subnet_id.value)
+  private_subnet_ids = split(",",  data.aws_ssm_parameter.private_subnet_ids.value)
 }
