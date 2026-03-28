616b968e0ff2ab86746bd06704601de412fbb85a
zakeer3551 <zak.ican4d@gmail.com>
2026-03-28 16:57:52 +0530

    deploying roboshop full project using terraform

--- 60-catalogue/bootstrap.sh
+++ 60-catalogue/bootstrap.sh
@@ -7,7 +7,7 @@ cd /home/ec2-user
 
 git clone https://github.com/Zakeer3551/ansible-roboshop-roles-tf.git
 
-cd /ansible-roboshop-roles-tf
+cd ansible-roboshop-roles-tf
 
 git pull
 ansible-playbook -e ${component} roboshop.yaml
